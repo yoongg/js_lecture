003e268e029d5d3cf86b11953780582b81d35e5f
yoongg <yoonna0912@gmail.com>
2023-02-23 10:32:29 +0900

    간단실습 3문제

---
+++ day4/test.js
@@ -0,0 +1,10 @@
+// 1. 
+$("li:nth-child(3)").css("background-color","yellow");
+$("ul li:eq(2)").css("background-color","yellow");
+
+// 2.
+$("li:nth-child(2n").css("color","red");
+
+// 3.
+$("ul").append("<li>내용4</li>");
+
